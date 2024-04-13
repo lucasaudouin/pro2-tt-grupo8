747ea88cfdd6554897ae6c9dbbbb0fcf4af569d5
lucasaudouin <laudouin@udesa.edu.ar>
2024-04-13 16:47:44 -0300

    realizo formularios de login y registro en ejs

---
+++ myapp/routes/vistas.js
@@ -0,0 +1,10 @@
+
+
+/*importar express y router */
+
+const express = require('express');
+const router = express.Router();
+const vistascontrollers = require('')
+
+router.get(',', vistascontrollers.index);
+
